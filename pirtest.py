cf5293b207bdf94f85d988eff3a43eea4d74a429
muhammad1928 <muhammadsn7@gmail.com>
2024-12-09 17:54:17 +0100

    test

---
+++ pirtest.py
@@ -0,0 +1,12 @@
+from gpizero import MotionSensor
+
+pir = MotionSensor(12)
+
+while True:
+    print("Scanning for motions...")
+    pir.wait_for_motion()
+
+    print("Motion detected!")
+    pir.wait_for_no_motion()
+
+    print("no motion detected.")
